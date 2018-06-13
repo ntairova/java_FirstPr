5bf0ea147ca54e7f42b84e394abd1f7f01fe82c5
ntairova <nelya.tairova@gmail.com>
2018-06-13 15:42:48 +0300

    wd updated

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactsHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactsHelper.java
@@ -1,13 +1,14 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactsHelper extends HelperBase {
 
 
-  public ContactsHelper(FirefoxDriver wd) {
+  public ContactsHelper(WebDriver wd) {
     super(wd);
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -1,13 +1,14 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SessionHelper extends HelperBase {
 
 
 
-  public SessionHelper(FirefoxDriver wd) {
+  public SessionHelper(WebDriver wd) {
     super(wd);
 
   }
